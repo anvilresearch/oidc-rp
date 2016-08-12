bb2357cd72d04e713a464a5396eaa6f05bb9282c
Christian Smith <smith@anvil.io>
2016-08-11 22:12:13 -0700

    wip(FormUrlEncoded): class definition

---
+++ src/FormUrlEncoded.js
@@ -0,0 +1,36 @@
+/**
+ * Dependencies
+ */
+
+/**
+ * FormUrlEncoded
+ */
+class FormUrlEncoded {
+
+  /**
+   * Encode
+   *
+   * @description
+   * Represent an object as x-www-form-urlencoded string.
+   *
+   * @param {Object} data
+   * @returns {string}
+   */
+  static encode (data) {}
+
+  /**
+   * Decode
+   *
+   * @description
+   * Parse a x-www-form-urlencoded into an object.
+   *
+   * @param {string} data
+   * @returns {Object}
+   */
+  static decode (data) {}
+}
+
+/**
+ * Export
+ */
+module.exports = FormUrlEncoded
