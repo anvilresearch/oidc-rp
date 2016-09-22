cefeb933420d81ee2a5470dde076e80ece2f792d
Christian Smith <smith@anvil.io>
2016-09-22 15:28:51 -0700

    wip(AuthenticationRequest): class definition

---
+++ src/AuthenticationRequest.js
@@ -0,0 +1,22 @@
+/**
+ * Dependencies
+ */
+
+/**
+ * Authentication Request
+ */
+class AuthenticationRequest {
+
+  /**
+   * Constructor
+   */
+  constructor (rp) {
+    Object.assign(this, rp)
+  }
+
+}
+
+/**
+ * Export
+ */
+module.exports = AuthenticationRequest
