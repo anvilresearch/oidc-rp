77cca5959a7998be723aa9b7d63a9d8c436d58f4
Christian Smith <smith@anvil.io>
2016-09-22 15:34:12 -0700

    wip(AuthenticationRequest): popup method

--- src/AuthenticationRequest.js
+++ src/AuthenticationRequest.js
@@ -14,6 +14,38 @@ class AuthenticationRequest {
     Object.assign(this, rp)
   }
 
+  /**
+   * popup
+   *
+   * @description
+   * Configure the authorize popup window
+   * Adapted from dropbox-js for ngDropbox
+   *
+   * @param {Number} popupWidth
+   * @param {Number} popupHeight
+   *
+   * @returns {string}
+   */
+  static popup (popupWidth, popupHeight) {
+    let x0, y0, width, height, popupLeft, popupTop
+
+    // Metrics for the current browser window.
+    x0 = window.screenX || window.screenLeft
+    y0 = window.screenY || window.screenTop
+    width = window.outerWidth || document.documentElement.clientWidth
+    height = window.outerHeight || document.documentElement.clientHeight
+
+    // Computed popup window metrics.
+    popupLeft = Math.round(x0) + (width - popupWidth) / 2
+    popupTop = Math.round(y0) + (height - popupHeight) / 2.5
+    if (popupLeft < x0) { popupLeft = x0 }
+    if (popupTop < y0) { popupTop = y0 }
+
+    return 'width=' + popupWidth + ',height=' + popupHeight + ',' +
+    'left=' + popupLeft + ',top=' + popupTop + ',' +
+    'dialog=yes,dependent=yes,scrollbars=yes,location=yes'
+  }
+
 }
 
 /**
