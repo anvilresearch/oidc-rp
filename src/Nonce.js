e76272bb06d8d94d2919050c9f444247b259ad4c
Christian Smith <smith@anvil.io>
2016-09-23 22:24:47 -0700

    wip(Nonce): generate method

---
+++ src/Nonce.js
@@ -0,0 +1,34 @@
+/**
+ * Dependencies
+ */
+const crypto = require('webcrypto')
+
+/**
+ * Nonce
+ */
+class Nonce {
+
+  /**
+   * generate
+   *
+   * @description
+   * Generate a base64url encoded SHA-256 hashed random nonce of a given length.
+   *
+   * @param {Number} nonce
+   * @returns {Promise}
+   */
+  static generate (length) {
+    let random = crypto.getRandomValues(new Uint32Array(length))
+    return crypto.subtle.digest('SHA-256', random).then(hash => base64url(hex(hash)))
+  }
+
+  /**
+   * verify
+   */
+  verify () {}
+}
+
+/**
+ * Export
+ */
+export default Nonce
