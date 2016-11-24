95fd142a859d54b64a9a6485e8c50345a4c73df9
Dmitri Zagidulin <dzagidulin@gmail.com>
2016-11-23 20:13:03 -0500

    fix(npm): Add src/index.js (npm main:)

---
+++ src/index.js
@@ -0,0 +1,14 @@
+'use strict'
+
+module.exports = {
+  AccessToken: require('./AccessToken'),
+  AccessTokenSchema: require('./AccessTokenSchema'),
+  AuthenticationRequest: require('./AuthenticationRequest'),
+  AuthenticationResponse: require('./AuthenticationResponse'),
+  FormUrlEncoded: require('./FormUrlEncoded'),
+  IDToken: require('./IDToken'),
+  IDTokenSchema: require('./IDTokenSchema'),
+  Nonce: require('./Nonce'),
+  RelyingParty: require('./RelyingParty'),
+  RelyingPartySchema: require('./RelyingPartySchema')
+}
