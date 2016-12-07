b6a9f2c25fa4bd0136f0e50c04b72e8a7c07813d
Dmitri Zagidulin <dzagidulin@gmail.com>
2016-12-07 17:22:42 -0500

    refactor(main): Export RelyingParty directly

--- src/index.js
+++ src/index.js
@@ -1,14 +1,3 @@
 'use strict'
 
-module.exports = {
-  AccessToken: require('./AccessToken'),
-  AccessTokenSchema: require('./AccessTokenSchema'),
-  AuthenticationRequest: require('./AuthenticationRequest'),
-  AuthenticationResponse: require('./AuthenticationResponse'),
-  FormUrlEncoded: require('./FormUrlEncoded'),
-  IDToken: require('./IDToken'),
-  IDTokenSchema: require('./IDTokenSchema'),
-  Nonce: require('./Nonce'),
-  RelyingParty: require('./RelyingParty'),
-  RelyingPartySchema: require('./RelyingPartySchema')
-}
+module.exports = require('./RelyingParty')
