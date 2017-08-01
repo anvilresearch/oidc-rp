93fe076ff72fd73ee548f873e4456147c349fbfc
Dmitri Zagidulin <dzagidulin@gmail.com>
2017-08-01 17:03:49 -0400

    misc(AuthenticationRequest): Reformat

--- src/AuthenticationRequest.js
+++ src/AuthenticationRequest.js
@@ -11,7 +11,6 @@ const URL = require('urlutils')
  * Authentication Request
  */
 class AuthenticationRequest {
-
   /**
    * create
    *
@@ -28,9 +27,12 @@ class AuthenticationRequest {
    * @returns {Promise}
    */
   static create (rp, options, session) {
-    return new Promise((resolve, reject) => {
-      let {provider, defaults, registration} = rp
+    const {provider, defaults, registration} = rp
 
+    let issuer, endpoint, client, params
+
+    return Promise.resolve()
+      .then(() => {
         // validate presence of OP configuration, RP client registration,
         // and default parameters
         assert(provider.configuration,
@@ -43,74 +45,72 @@ class AuthenticationRequest {
           'RelyingParty client registration is missing')
 
         // define basic elements of the request
-        let issuer = provider.configuration.issuer
-        let endpoint = provider.configuration.authorization_endpoint
-        let client = { client_id: registration.client_id}
-        let params = Object.assign(defaults.authenticate, client, options)
+        issuer = provider.configuration.issuer
+        endpoint = provider.configuration.authorization_endpoint
+        client = { client_id: registration.client_id}
+        params = Object.assign(defaults.authenticate, client, options)
 
         // validate presence of required configuration and parameters
         assert(issuer,
-            'Missing issuer in provider OpenID Configuration')
+          'Missing issuer in provider OpenID Configuration')
 
         assert(endpoint,
-            'Missing authorization_endpoint in provider OpenID Configuration')
+          'Missing authorization_endpoint in provider OpenID Configuration')
 
         assert(params.scope,
-            'Missing scope parameter in authentication request')
+          'Missing scope parameter in authentication request')
 
         assert(params.response_type,
-            'Missing response_type parameter in authentication request')
+          'Missing response_type parameter in authentication request')
 
         assert(params.client_id,
-            'Missing client_id parameter in authentication request')
+          'Missing client_id parameter in authentication request')
 
         assert(params.redirect_uri,
-            'Missing redirect_uri parameter in authentication request')
+          'Missing redirect_uri parameter in authentication request')
 
         // generate state and nonce random octets
         params.state = Array.from(crypto.getRandomValues(new Uint8Array(16)))
         params.nonce = Array.from(crypto.getRandomValues(new Uint8Array(16)))
-
 
         // hash the state and nonce parameter values
         return Promise.all([
           crypto.subtle.digest({ name: 'SHA-256' }, new Uint8Array(params.state)),
           crypto.subtle.digest({ name: 'SHA-256' }, new Uint8Array(params.nonce))
         ])
+      })
 
-        // serialize the request with original values, store in session by
-        // encoded state param, and replace state/nonce octets with encoded
-        // digests
-        .then(digests => {
-          let state = base64url(Buffer.from(digests[0]))
-          let nonce = base64url(Buffer.from(digests[1]))
-          let key = `${issuer}/requestHistory/${state}`
+      // serialize the request with original values, store in session by
+      // encoded state param, and replace state/nonce octets with encoded
+      // digests
+      .then(digests => {
+        let state = base64url(Buffer.from(digests[0]))
+        let nonce = base64url(Buffer.from(digests[1]))
+        let key = `${issuer}/requestHistory/${state}`
 
-          // store the request params for response validation
-          // with serialized octet values for state and nonce
-          session[key] = JSON.stringify(params)
+        // store the request params for response validation
+        // with serialized octet values for state and nonce
+        session[key] = JSON.stringify(params)
 
-          // replace state and nonce octets with base64url encoded digests
-          params.state = state
-          params.nonce = nonce
-        })
+        // replace state and nonce octets with base64url encoded digests
+        params.state = state
+        params.nonce = nonce
+      })
 
-        // optionally encode a JWT with the request parameters
-        .then(() => {
-          // TODO
-          // optionally encode the request parameters as a JWT
-          // and replace params with `{ request: <jwt> }`
-        })
+      // optionally encode a JWT with the request parameters
+      .then(() => {
+        // TODO
+        // optionally encode the request parameters as a JWT
+        // and replace params with `{ request: <jwt> }`
+      })
 
-        // render the request URI and terminate the algorithm
-        .then(() => {
-          let url = new URL(endpoint)
-          url.search = FormUrlEncoded.encode(params)
-          resolve(url.href)
-        })
-    })
+      // render the request URI and terminate the algorithm
+      .then(() => {
+        let url = new URL(endpoint)
+        url.search = FormUrlEncoded.encode(params)
+        resolve(url.href)
+      })
   }
-
 }
 
 /**
